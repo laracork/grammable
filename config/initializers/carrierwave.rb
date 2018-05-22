caf39fddd513e799c6643b37dd74c5e79b11eb00
Lara Corkrey <laracorkrey@gmail.com>
2018-05-22 01:03:46 +0000

    added validation for images

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,4 +1,3 @@
-
 # config/initializers/carrierwave.rb
 
 CarrierWave.configure do |config|
@@ -8,5 +7,5 @@ CarrierWave.configure do |config|
     aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
     aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
+  config.fog_directory = ENV["AWS_BUCKET"]              # required
 end
